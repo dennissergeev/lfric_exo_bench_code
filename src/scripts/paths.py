d153742b672dc8cdd5b9ad67d02fd4faab235ca6
Denis Sergeev <dennis.sergeev@gmail.com>
2022-11-02 10:26:07 +0000

    change paths.py

---
+++ src/scripts/paths.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""Common paths useful for manipulating datasets and generating figures."""
+from pathlib import Path
+
+# Absolute path to the top level of the repository
+root = Path(__file__).resolve().parents[2].absolute()
+
+# Absolute path to the `src` folder
+src = root / "src"
+
+# Absolute path to the `src/static` folder (contains static images)
+static = src / "static"
+
+# Absolute path to the `src/scripts` folder (contains figure/pipeline scripts)
+scripts = src / "scripts"
+
+# Absolute path to the `src/figures` folder (contains figure output)
+figures = src / "figures"
+
+# Constants
+const = scripts / "const"
+
+# Absolute path to the `src/data` folder (contains datasets)
+# data = src / "data"
+data = root.parent / "data"
+
+# UM output
+results_um = data / "raw" / "um"
+
+# LFRic output
+results_lfric = data / "raw" / "lfric"
