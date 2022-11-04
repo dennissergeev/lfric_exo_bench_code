5a4b26b829b630e854a547b31a4f04db440d8a83
Denis Sergeev <dennis.sergeev@gmail.com>
2022-11-04 13:31:03 +0000

    add a module with quick-plot functions

---
+++ src/scripts/quickplot.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Quickplot functions for the UM output."""
+import matplotlib.pyplot as plt
+from aeolus.model import um
+from aeolus.coord import get_cube_rel_days
+
+
+def qplt_map(cube, ax=None, model=um, **kw_plt):
+    newax = False
+    if ax is None:
+        ax = plt.axes()
+        newax = True
+    fig = ax.figure
+    lons = cube.coord(um.x).points
+    lats = cube.coord(um.y).points
+    mappable = ax.pcolormesh(lons, lats, cube.data, **kw_plt)
+    fig.colorbar(mappable, ax=ax)
+    if newax:
+        return ax
+
+
+def qplt_timeseries_1d(cube, ax=None, model=um, **kw_plt):
+    newax = False
+    if ax is None:
+        ax = plt.axes()
+        newax = True
+    days = get_cube_rel_days(cube, model=um)
+    ax.plot(days, cube.data, **kw_plt)
+    if newax:
+        return ax
+
+
+def qplt_timeseries_2d(cube, ax=None, model=um, **kw_plt):
+    newax = False
+    if ax is None:
+        ax = plt.axes()
+        newax = True
+    fig = ax.figure
+    days = get_cube_rel_days(cube, model=um)
+    z = cube.coord(um.z).points
+    mappable = ax.pcolormesh(days, z, cube.data.T, **kw_plt)
+    fig.colorbar(mappable, ax=ax)
+    if newax:
+        return ax
