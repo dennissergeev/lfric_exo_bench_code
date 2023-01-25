07a90f820e2a2f67a70fee8bb86ddc0b5cc640bf
Denis Sergeev <dennis.sergeev@gmail.com>
2023-01-25 17:03:17 +0000

    add a script to spatially aggregate raw LFRic data

---
+++ src/scripts/aggr_raw_lfric_data.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Perform spatial reduction operations on raw LFRic output."""
+
+# Commonly used standard library tools
+import argparse
+from functools import partial
+from pathlib import Path
+from time import time
+import warnings
+
+# Sci stack
+import iris
+
+# My packages and local scripts
+from aeolus.const import add_planet_conf_to_cubes, init_const
+from aeolus.coord import get_cube_rel_days
+from aeolus.io import save_cubelist
+from aeolus.subset import DimConstr, unique_cubes
+import paths
+
+from pouch.log import create_logger
+
+from lfric_model import lfric
+from lfric_util import (
+    add_equally_spaced_height_coord,
+    add_um_height_coord,
+    load_lfric_raw,
+    ugrid_spatial,
+)
+
+# Global definitions and styles
+warnings.filterwarnings("ignore")
+SCRIPT = Path(__file__).name
+
+AGGREGATORS = ["max", "mean", "median", "min", "std_dev", "variance"]
+
+
+def parse_args(args=None):
+    """Argument parser."""
+    ap = argparse.ArgumentParser(
+        SCRIPT,
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        epilog=f"""Usage:
+./{SCRIPT} -p earth -l trap1e -i ~/path/to/inp/dir/ -o ~/path/to/out/dir
+""",
+    )
+
+    ap.add_argument("-i", "--inpdir", type=str, help="Input directory")
+    ap.add_argument("-o", "--outdir", type=str, help="Output directory")
+    ap.add_argument(
+        "-l", "--label", type=str, required=True, help="Simulation label"
+    )
+    ap.add_argument(
+        "-p", "--planet", type=str, required=True, help="Planet configuration"
+    )
+    ap.add_argument(
+        "-c",
+        "--cnum",
+        type=str,
+        help="Cubed Sphere Mesh Number",
+        default="C48",
+    )
+    ap.add_argument(
+        "--level_height",
+        type=str,
+        default="uniform",
+        help="Type of the vertical level height coordinate",
+    )
+    return ap.parse_args(args)
+
+
+def main(args=None):
+    """Main entry point of the script."""
+    t0 = time()
+    L = create_logger(Path(__file__))
+    # Parse command-line arguments
+    args = parse_args(args)
+    planet = args.planet
+    L.info(f"{planet=}")
+
+    label = args.label
+    L.info(f"{label=}")
+
+    # Cubed sphere mesh C number
+    c_num = args.cnum
+
+    # Height coordinate
+    if args.level_height == "uniform":
+        add_levs = partial(
+            add_equally_spaced_height_coord, model_top_height=32_000
+        )
+    elif args.level_height == "um_L38_29t_9s_40km":
+        add_levs = partial(
+            add_um_height_coord,
+            path_to_levels_file=paths.vert / "vertlevs_L38_29t_9s_40km",
+        )
+    else:
+        raise ValueError(f"level_height={args.level_height} is not valid.")
+
+    # Input directory
+    # inpdir = mypaths.sadir / label
+    inpdir = Path(args.inpdir)
+    L.info(f"{inpdir=}")
+
+    # Create a subdirectory for processed data
+    outdir = Path(args.outdir)
+    # outdir = mypaths.sadir / label / "_processed"
+    outdir.mkdir(parents=True, exist_ok=True)
+
+    # Make a list of files matching the file mask and the start day threshold
+    fnames = sorted(
+        inpdir.glob(f"*/*{c_num}*/lfric_diag.nc"),
+        key=lambda x: int(x.parent.parent.name),
+    )
+    if len(fnames) == 0:
+        L.critical("No files found!")
+        return
+    L.info(f"fnames({len(fnames)}) = {fnames[0]} ... {fnames[-1]}")
+
+    cl_raw = load_lfric_raw(fnames, callback=add_levs)
+    if len(cl_raw) == 0:
+        L.critical("Files are empty!")
+        return
+    # Process only single-level cubes
+    DC = DimConstr(model=lfric)
+    cubes_single_lev = unique_cubes(
+        cl_raw.extract(DC.relax.t).extract(
+            iris.Constraint(cube_func=lambda cube: cube.ndim == 2)
+        )
+    )
+
+    # Loop over cubes and operators
+    cl_proc = iris.cube.CubeList()
+    for cube in cubes_single_lev:
+        L.info(f"Processing {cube.var_name}")
+        for aggr in AGGREGATORS:
+            L.info(f"Applying {aggr=}")
+            cl_proc.append(ugrid_spatial(cube, aggr, model=lfric))
+
+    const = init_const(planet, directory=paths.const)
+    add_planet_conf_to_cubes(cl_proc, const=const)
+
+    time_prof = "inst"
+    # Write the data to a netCDF file
+    gl_attrs = {
+        "name": label,
+        "planet": planet,
+        "processed": "True",
+    }
+    days = 0 + get_cube_rel_days(cl_proc[0]).astype(int)
+    day_str = f"days{days[0]}"
+    if len(days) > 1:
+        day_str += f"_{days[-1]}"
+    fname_out = outdir / f"{label}_{time_prof}_{day_str}_aggr.nc"
+    save_cubelist(cl_proc, fname_out, **gl_attrs)
+    L.success(f"Saved to {fname_out}")
+    L.info(f"Execution time: {time() - t0:.1f}s")
+
+
+if __name__ == "__main__":
+    main()
